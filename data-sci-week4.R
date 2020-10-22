0c72c44d8ebee69d2d3412f51d12f8219c1e0885
Fay Bennett <feb518@york.ac.uk>
2020-10-22 16:40:33 +0100

    updating to newest version

--- data-sci-week4.R
+++ data-sci-week4.R
@@ -22,7 +22,7 @@ t.test(data = chaff, mass ~ sex, paired = TRUE)
 # t = -2.5168, df = 19, p-value = 0.02098
 # so there is a significant difference in mass between male and female chaffinches.
 
-ggplot(data = chaffsummary, aes(x = sex, y = mass) ) +
+fig1 <- ggplot(data = chaffsummary, aes(x = sex, y = mass) ) +
   geom_errorbar(aes( ymin = mass, 
                      ymax = mass), 
                 width = .5, 
@@ -36,31 +36,16 @@ ggplot(data = chaffsummary, aes(x = sex, y = mass) ) +
   theme(axis.line.x = element_line(colour = "black"),
         axis.line.y = element_line(colour = "black"))
 
-ggplot() +
-  geom_point(data = chaffsummary, aes(x = sex, y = mass)) +
-  geom_errorbar(chaffsummary, 
-                aes(ymin = mass, 
-                    ymax = mass), 
-                width = 0.2, size = 1) + 
-  geom_errorbar(chaffsummary,
-                aes(ymin = mass - se, 
-                    ymax = mass + se), 
-                width = 0.3) +
-  theme(panel.background = element_rect(fill = "white")) +
-  theme(axis.line.x = element_line(colour = "black"),
-        axis.line.y = element_line(colour = "black"))
+units <- "in"  
+fig_w <- 3.5
+fig_h <- fig_w
+dpi <- 300
+device <- "tiff" 
 
-ggplot() +
-  geom_point(data = chaff, aes(x = sex, y = mass)) +
-  geom_jitter(width = 0.1, colour = "#8c8c8c") +
-  geom_errorbar(data = chaffsummary, 
-                aes(ymin = mass - se, ymax = mass + se),
-                width = 0.3) +
-  geom_errorbar(data = chaffsummary, 
-                aes(ymin = mass, ymax = mass),
-                width = 0.2) +
-  ylab("Chaffinch Mass") +
-  xlab(NULL) +
-  ylim(0, 30) +
-  scale_x_discrete(labels = c("Male", "Female")) +
-  theme_classic()
+ggsave("fig1",
+       plot = fig1,
+       device = device,
+       width = fig_w,
+       height = fig_h,
+       units = units,
+       dpi = dpi)
