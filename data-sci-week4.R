1c690a57a2a93bc733fc6326c669c9a9b9d91dfe
Fay Bennett <feb518@york.ac.uk>
2020-10-22 16:10:25 +0100

    figure for week 4 workshop

--- data-sci-week4.R
+++ data-sci-week4.R
@@ -1,6 +1,7 @@
 # loading required packages
 library(ggplot2)
 library(Rmisc)
+library(ggpubr)
 
 # loading in the tidy chaff data:
 chaff <- read.table("chaff.txt", header = T)
@@ -21,23 +22,33 @@ t.test(data = chaff, mass ~ sex, paired = TRUE)
 # t = -2.5168, df = 19, p-value = 0.02098
 # so there is a significant difference in mass between male and female chaffinches.
 
+ggplot(data = chaffsummary, aes(x = sex, y = mass) ) +
+  geom_errorbar(aes( ymin = mass, 
+                     ymax = mass), 
+                width = .5, 
+                colour = "red",
+                size = 1) +
+  geom_errorbar(aes( ymin = mass - se, 
+                     ymax = mass + se), 
+                width = .7, 
+                colour = "red") +
+  theme(panel.background = element_rect(fill = "white")) +
+  theme(axis.line.x = element_line(colour = "black"),
+        axis.line.y = element_line(colour = "black"))
+
 ggplot() +
-  geom_point(data = chaff, aes(x = sex, y = mass)) +
-  geom_errorbar(data = chaffsummary, 
+  geom_point(data = chaffsummary, aes(x = sex, y = mass)) +
+  geom_errorbar(chaffsummary, 
                 aes(ymin = mass, 
                     ymax = mass), 
                 width = 0.2, size = 1) + 
-  geom_errorbar(data = chaffsummary,
+  geom_errorbar(chaffsummary,
                 aes(ymin = mass - se, 
-                     ymax = mass + se), 
+                    ymax = mass + se), 
                 width = 0.3) +
   theme(panel.background = element_rect(fill = "white")) +
   theme(axis.line.x = element_line(colour = "black"),
-        axis.line.y = element_line(colour = "black")) +
-  ylab("Chaffinch Mass") +
-  xlab(NULL) +
-  ylim(0, 30) +
-  scale_x_discrete(labels = c("Male", "Female"))
+        axis.line.y = element_line(colour = "black"))
 
 ggplot() +
   geom_point(data = chaff, aes(x = sex, y = mass)) +
